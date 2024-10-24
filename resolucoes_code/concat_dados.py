3de109c4e80354cdc3f125a87fa844b39898be76
Wagner Andrade <52001930+wsawebmaster@users.noreply.github.com>
2024-10-24 18:25:22 -0300

    feat: Update concat_dados.py

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,8 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenada = info1 + " " + info2
+
+print("As informações concatenads são: ", info_concatenada)
